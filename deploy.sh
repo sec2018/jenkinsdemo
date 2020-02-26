10bcf13a325597ba309dd3f76a3bca7cb5acf647
chen <2509796848@qq.com>
2020-02-26 22:17:01 +0800

    test 02 add script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+killTomcat()
+{
+    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+    echo "tomcat Id list :$pid"
+    if [ "$pid" = "" ]
+    then
+        echo "no tomcat pid alive"
+    else
+        kill -9 $pid
+    fi
+}
+
+cd $PROJ_PATH/jenkinsdemo
+mvn clean install
+
+killtomcat
+
+rm -rf $TOMCAT_APP_PATH/webapps/jenkinsdemo
+rm -f $TOMCAT_APP_PATH/webapps/jenkinsdemo.war
+
+cp $PROJ_PATH/jenkinsdemo/target/jenkinsdemo-0.0.1-SNAPSHOT.war $TOMCAT_APP_PATH/webapps/jenkinsdemo-0.0.1-SNAPSHOT.war
+
+cd $TOMCAT_APP_PATH/webapps/
+mv jenkinsdemo-0.0.1-SNAPSHOT.war jenkinsdemo.war
+
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
