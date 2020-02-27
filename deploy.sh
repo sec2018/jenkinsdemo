83c2981ae7da76e4eb5d72f27658c8750016109a
chen <2509796848@qq.com>
2020-02-27 22:43:10 +0800

    test 19

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-export BUILD_ID=dontkillme
+export BUILD_ID="dontkillme"
 
 killTomcat()
 {
     pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
     echo "tomcat Id list :$pid"
-    if [ -n "$pid" ]
+    if [ "$pid"="" ]
     then
+        echo "no tomcat alive"
+    else
         kill -9 $pid
     fi
 }
@@ -19,9 +21,8 @@ killTomcat()
 cd $PROJ_PATH/jenkinsdemo
 mvn clean package -Dmaven.test.skip=true
 
-# killTomcat
-# cd $TOMCAT_APP_PATH/
-# sh bin/shutdown.sh
+killTomcat
+sleep(3)
 
 rm -rf $TOMCAT_APP_PATH/webapps/jenkinsdemo
 rm -f $TOMCAT_APP_PATH/webapps/jenkinsdemo.war
@@ -31,5 +32,5 @@ cp $PROJ_PATH/jenkinsdemo/target/jenkinsdemo-0.0.1-SNAPSHOT.war $TOMCAT_APP_PATH
 cd $TOMCAT_APP_PATH/webapps/
 mv jenkinsdemo-0.0.1-SNAPSHOT.war jenkinsdemo.war
 
-# cd $TOMCAT_APP_PATH/
-# sh bin/startup.sh
+cd $TOMCAT_APP_PATH/
+sh bin/startup.sh
