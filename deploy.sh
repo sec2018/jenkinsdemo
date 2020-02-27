f072edf7c2a334716259f263d12c1a99377ac053
chen <2509796848@qq.com>
2020-02-27 22:49:16 +0800

    test 21

--- deploy.sh
+++ deploy.sh
@@ -26,11 +26,14 @@ mvn clean package -Dmaven.test.skip=true
 killTomcat
 sleep 3
 
+echo "rm"
 rm -rf $TOMCAT_APP_PATH/webapps/jenkinsdemo
 rm -f $TOMCAT_APP_PATH/webapps/jenkinsdemo.war
 
+echo "cp"
 cp $PROJ_PATH/jenkinsdemo/target/jenkinsdemo-0.0.1-SNAPSHOT.war $TOMCAT_APP_PATH/webapps/jenkinsdemo-0.0.1-SNAPSHOT.war
 
+echo "mv"
 cd $TOMCAT_APP_PATH/webapps/
 mv jenkinsdemo-0.0.1-SNAPSHOT.war jenkinsdemo.war
 
